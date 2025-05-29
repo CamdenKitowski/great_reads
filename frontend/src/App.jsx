aabe6dfcfd927f5dce59751ed7055a9597e2ee27
camkit <camdenkitowski@yahoo.com>
2025-05-28 19:36:08 -0700

    navbar update + css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@
 import './App.css'
 import Home from './pages/Home'
 import NavBar from './components/NavBar'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   
   return (
     <>
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
