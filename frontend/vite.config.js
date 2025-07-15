285dddbbe2414e51f8138f50620f737b3f8302a2
camdenkitowski <camdenkitowski@yahoo.com>
2025-07-15 16:05:39 -0700

    plz work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   build: {
+//     outDir: 'dist'
+//   },
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
 })
