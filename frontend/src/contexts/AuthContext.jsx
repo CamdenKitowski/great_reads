49392fb5e4e3869964c9dbb0f5c609bfa3cf09a8
camdenkitowski <camdenkitowski@yahoo.com>
2025-07-15 17:22:36 -0700

    update base url for new backend project

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -4,7 +4,7 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const API_BASE_URL = 'https://great-reads-bookshelf.vercel.app'
+    const API_BASE_URL = 'https://great-reads-backend.vercel.app/' // backend project URL
     // const API_BASE_URL = 'http://localhost:3001'; // this is where the backend server is running -- need to change this
 
     useEffect(() => {
